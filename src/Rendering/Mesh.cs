de70c0dacad80afa0bb44aea5b7cf1d5130ab6d2
Deakin Simpson <deakinsimpson976@gmail.com>
2026-08-02 21:17:29 +1000

    added comment to Mesh.cs

---
+++ src/Rendering/Mesh.cs
@@ -0,0 +1,14 @@
+/*
+This calss will be used to hold all mesh data
+
+this is so that objects can have object specific data
+but keep the mesh data (verticies indicies etc) stored
+in its own class
+
+This will also hold any Mesh related functions
+*/
+
+public class Mesh
+{
+    
+}
